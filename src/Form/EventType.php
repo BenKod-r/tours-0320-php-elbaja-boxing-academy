f1f7758348a0df0d2ba25dfea15a19ee146adef1
khaleddwm <benk.devweb@gmail.com>
2020-06-05 15:58:05 +0200

    Correction HTML and type of form fields of the EvenType class

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,17 +6,19 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('dateStart')
-            ->add('dateEnd')
-            ->add('poster')
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('dateStart', DateType::class)
+            ->add('dateEnd', DateType::class)
+            ->add('poster', TextType::class)
         ;
     }
 
