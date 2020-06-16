e61ad41b8c4d98d995bd21a94a348a9083baa6b3
DSARRAZY <d.sarrazy@gmail.com>
2020-06-16 14:54:02 +0200

    Correction bug

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -6,17 +6,31 @@ use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('dateStart')
-            ->add('dateEnd')
-            ->add('poster')
+            ->add('name', TextType::class, [
+                'required'=> true
+            ])
+            ->add('description', TextType::class, [
+                'required'=> true
+            ])
+            ->add('dateStart', DateType::class, [
+                 'required'=> true,
+                 'widget' => 'single_text'
+            ])
+            ->add('dateEnd', DateType::class, [
+                  'required'=> false,
+                  'widget' => 'single_text'
+            ])
+            ->add('poster', TextType::class, [
+                'required'=> false
+            ])
         ;
     }
 
