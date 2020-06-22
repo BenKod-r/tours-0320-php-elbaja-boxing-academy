9a22c3ee4ea6ed6cbe16a1fcfbd020a89085c231
DSARRAZY <d.sarrazy@gmail.com>
2020-06-22 14:27:43 +0200

    sauvegarde

---
+++ src/Controller/HallOfFameController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/hall")
+ */
+class HallOfFameController extends AbstractController
+{
+    /**
+     * Hall of Fame page display
+     * @Route("/",name="hall_index")
+     * @return Response A response instance
+     */
+    public function hall() :Response
+    {
+        return $this->render('hall/index.html.twig');
+    }
+}
