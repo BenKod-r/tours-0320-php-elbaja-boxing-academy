792075d9dd1569ca111a9067e271e7873d0716ab
chalumeau <chalumeauk@gmail.com>
2020-06-23 17:06:59 +0200

    sauvegarde

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use App\Model\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * Contact page display
+     * @Route("/contact",name="contact_index",  methods={"GET"})
+     * @return Response A reponse instance
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+}
