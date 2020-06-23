7709980230a3c16b0a2894de16eed194765bc318
chalumeau <chalumeauk@gmail.com>
2020-06-23 15:52:57 +0200

    créationModel->Contact et ContactType

---
+++ src/Form/ContactType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Model\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('subject', TextType::class)
+            ->add('description', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
