94d065cda28df836397574e162d692e44667ebe7
khaleddwm <benk.devweb@gmail.com>
2020-07-07 11:04:12 +0200

    Add choicePoster method, addPoster, and comment on methods

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * Auteur: Khaled Benharrat, Damien Sarrazy, Kevin Chalumeau
+ * Date: 30/06/2020
+ */
 
 namespace App\Controller;
 
 use App\Entity\Event;
+use App\Entity\Poster;
+use App\Repository\PosterRepository;
 use App\Form\EventType;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,16 +23,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class EventController extends AbstractController
 {
     /**
+     * Returns all events
+     * 
      * @Route("/", name="event_index", methods={"GET"})
      */
     public function index(EventRepository $eventRepository): Response
     {
         return $this->render('event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
+            'events' => $eventRepository->findBy([], ['id' => 'desc']),
         ]);
     }
 
     /**
+     * Returns a form to create an event and redirection to choose a poster
+     * 
      * @Route("/new", name="event_new", methods={"GET","POST"})
      * @IsGranted("ROLE_ADMIN")
      */
@@ -41,7 +51,7 @@ class EventController extends AbstractController
             $entityManager->persist($event);
             $entityManager->flush();
 
-            return $this->redirectToRoute('event_index');
+            return $this->redirectToRoute('event_poster_index', ['id' => $event->getId()]);
         }
 
         return $this->render('event/new.html.twig', [
@@ -51,6 +61,37 @@ class EventController extends AbstractController
     }
 
     /**
+     * List of all posters, choice of a poster for an event
+     * 
+     * @Route("/new/{id}", name="event_poster_index", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function choicePoster(Event $event, PosterRepository $posterRepository): Response
+    {
+        return $this->render('event/poster.html.twig', [
+            'posters' => $posterRepository->findBy([], ['id' => 'desc']),
+            'event' => $event,
+        ]);
+    }
+
+    /**
+     * Add a poster to an event and redirection to list of poster
+     * 
+     * @Route("/new/{event}/poster/{poster}", name="event_new_poster", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function addPoster(Event $event, Poster $poster): Response
+    {
+        $event->setPoster($poster);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('event_index');
+    }
+
+    /**
+     * Return an event
+     * 
      * @Route("/{id}", name="event_show", methods={"GET"})
      */
     public function show(Event $event): Response
@@ -61,6 +102,8 @@ class EventController extends AbstractController
     }
 
     /**
+     * Returns an event and a form to edit the event
+     * 
      * @Route("/{id}/edit", name="event_edit", methods={"GET","POST"},requirements={"id": "\d+"})
      * @IsGranted("ROLE_ADMIN")
      */
@@ -82,6 +125,8 @@ class EventController extends AbstractController
     }
 
     /**
+     * Delete an event
+     * 
      * @Route("/{id}", name="event_delete", methods={"DELETE"})
      * @IsGranted("ROLE_ADMIN")
      */
