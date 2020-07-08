bdd56ae666b1258e28c400700589188c1512c3ba
DSARRAZY <d.sarrazy@gmail.com>
2020-07-08 14:49:31 +0200

    correction TRAVIS

--- assets/js/poster.js
+++ assets/js/poster.js
@@ -1,1 +1,2 @@
-$("input[type=file]").change(function (e){$(this).next('.custom-file-label').text(e.target.files[0].name);})
+/* global upload, upload */
+$('input[type=file]').change(function (e) { $(this).next('.custom-file-label').text(e.target.files[0].name); });
