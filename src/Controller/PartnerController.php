c3abe051fbc272b606a56720263311cff5cdfed5
chalumeau <chalumeauk@gmail.com>
2020-07-09 16:15:44 +0200

    réalisation insertion partenaire en admin

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class PartnerController extends AbstractController
 {
@@ -20,5 +21,44 @@ class PartnerController extends AbstractController
         return $this->render('partner/index.html.twig', [
             'partners' => $partnerRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="partner_new", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function new(Request $request): Response
+    {
+        $partner = new Partner();
+        $form = $this->createForm(PartnerType::class, $partner);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($partner);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('partner_index');
+        }
+
+        return $this->render("partner/new.html.twig", [
+            'partner' => $partner,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="partner_delete", methods={"DELETE"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function delete(Request $request, Partner $partner): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$$partner->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($partner);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('partner_index');
     }
 }
