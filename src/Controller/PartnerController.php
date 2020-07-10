1d979687929400757bd90a0a74339dc9a49fd743
chalumeau <chalumeauk@gmail.com>
2020-07-10 16:32:56 +0200

    modif partenaire

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -3,18 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Partner;
+use App\Entity\Poster;
 use App\Form\PartnerType;
 use App\Repository\PartnerRepository;
+use App\Repository\PosterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @Route("/partner")
+ */
 class PartnerController extends AbstractController
 {
     /**
-     * @Route("/partner", name="partner_index", methods={"GET"})
+     * @Route("/", name="partner_index", methods={"GET"})
      */
     public function index(PartnerRepository $partnerRepository): Response
     {
@@ -38,7 +43,7 @@ class PartnerController extends AbstractController
             $entityManager->persist($partner);
             $entityManager->flush();
 
-            return $this->redirectToRoute('partner_index');
+            return $this->redirectToRoute('partner_poster_index', ['id' => $partner->getId()]);
         }
 
         return $this->render("partner/new.html.twig", [
@@ -46,6 +51,45 @@ class PartnerController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * List of all posters, choice of a poster for an partner
+     * @Route("/new/{id}", name="partner_poster_index", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function choicePoster(Partner $partner, PosterRepository $posterRepository): Response
+    {
+        return $this->render('partner/poster.html.twig', [
+            'posters' => $posterRepository->findBy([], ['date' => 'desc']),
+            'partner' => $partner,
+        ]);
+    }
+
+    /**
+     * Add a poster to an partner and redirection to list of poster
+     * @Route("/new/{partner}/poster/{poster}", name="partner_new_poster", methods={"GET","POST"})
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function addPoster(Partner $partner, Poster $poster): Response
+    {
+        $partner->setPoster($poster);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('partner_index');
+    }
+
+    /**
+     * Return an partner
+     * @Route("/{id}", name="partner_show", methods={"GET"})
+     */
+    public function show(Partner $partner): Response
+    {
+        return $this->render('partner/show.html.twig', [
+            'partner' => $partner,
+        ]);
+    }
+
 
     /**
      * @Route("/{id}", name="partner_delete", methods={"DELETE"})
